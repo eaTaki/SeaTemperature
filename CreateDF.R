6671d3325e554ea7976b7595b278ce9cc29960c4
Enrique Takiguchi <ea.takiguchi@gmail.com>
2025-11-26 23:25:36 +0100

    fixed numericals

--- CreateDF.R
+++ CreateDF.R
@@ -1,8 +1,9 @@
+library(readr)
 
-df_yearly = data.frame()
-df_cum = data.frame()
+sea.deep = data.frame()
+sea.pred = data.frame()
+
 years = seq(2000, 2017)
-
 csv_cols = c('row', 'col', 'ref', 'value')
 col_names = c('mes', 'fondaria', 'any', 'temperatura')
 
@@ -15,12 +16,19 @@ for (y in years){
   df = df[csv_cols]
   names(df) = col_names
   df['any'] = y
-  df_yearly <- rbind(df_yearly,df)
+  sea.deep <- rbind(sea.deep,df)
   
   # Averaged preceding
   df <- read.csv(paste("https://api.idescat.cat/rc/v1/aec/15196/2/ssv?t=", y, '00', sep = ''), sep=';')
   df = df[csv_cols]
   names(df) = col_names
   df['any'] = y
-  df_cum <- rbind(df_cum,df)
+  sea.pred <- rbind(sea.pred,df)
 }
+
+# Fixing format and making numerical
+sea.deep$fondaria <- parse_number(sea.deep$fondaria)
+sea.deep$temperatura <- as.numeric(gsub(",", ".", sea.deep$temperatura, fixed = TRUE))
+sea.pred$fondaria <- parse_number(sea.pred$fondaria)
+sea.pred$temperatura <- as.numeric(gsub(",", ".", sea.pred$temperatura, fixed = TRUE))
+
